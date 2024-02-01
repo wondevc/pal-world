7390da88502cddf91878fdd51064cbb3b5126559
채지원 <112457971+wondevc@users.noreply.github.com>
2024-02-01 21:28:10 +0900

    create windows update script

---
+++ windows/update-scripts/PalworldUpdate.bat
@@ -0,0 +1,48 @@
+@echo off
+setlocal
+
+set "log_file_name=UpdateLog.txt"
+set "log_directory=%cd%\logs\%DATE%"
+set "log_file_path=%log_directory%\%log_file_name%"
+
+if not exist "%log_directory%" (
+    mkdir "%log_directory%"
+)
+
+set "steamcmd_directory=..\..\SteamCMD"
+set "steamcmd_path=%steamcmd_directory%\steamcmd.exe"
+
+set "app_id=2394010"
+
+set "seven_zip_path=C:\Program Files\7-Zip\7z.exe"
+
+set "steamcmd_zip_path=.\steamcmd.zip"
+
+if not exist "%steamcmd_directory%" (
+    mkdir "%steamcmd_directory%"
+    echo [%DATE% %TIME%] Created steamcmd directory. >> %log_file_path%
+)
+
+if not exist "%steamcmd_path%" (
+    if not exist "%seven_zip_path%" (
+        echo [%DATE% %TIME%] Backup failed because 7-Zip is not installed. >> %log_file_path%
+
+        endlocal
+
+        exit /b
+    )
+
+    curl -o %steamcmd_zip_path% https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip
+
+    "%seven_zip_path%" x "%steamcmd_zip_path%" -o"%steamcmd_directory%" -y
+
+    del "%steamcmd_zip_path%"
+)
+
+start "" /wait %steamcmd_path% +login anonymous +app_update %app_id% validate +quit
+
+echo [%DATE% %TIME%] Update Completed. >> %log_file_path%
+
+endlocal
+
+exit /b
